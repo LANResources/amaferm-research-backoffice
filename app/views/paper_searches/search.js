b57f2a8de07cd67fbbb37275f0c5f7357a5ef2a1
Nick Reed <reednj77@gmail.com>
2014-02-28 16:35:36 -0600

    Handle blank search

--- app/views/paper_searches/search.js
+++ app/views/paper_searches/search.js
@@ -1,3 +1,3 @@
-$results = $("<%= escape_javascript(render partial: 'paper_searches/results', locals: { results: policy_scope(@paper_search.results) }) %>");
+$results = $("<%= escape_javascript(render partial: 'paper_searches/results', locals: { results: policy_scope(@paper_search.results || Paper.none) }) %>");
 $('#results').html($results);
 $('#new_paper_search').parents('.box').find('h2 i').replaceWith($('<i class="fa fa-search"></i>'));
