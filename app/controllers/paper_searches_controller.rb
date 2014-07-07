0aa6d05597f51c4843370ad2bd18e5ac5b1bb94a
Nick Reed <reednj77@gmail.com>
2014-07-07 12:08:15 -0500

    Paper search export

--- app/controllers/paper_searches_controller.rb
+++ app/controllers/paper_searches_controller.rb
@@ -4,7 +4,7 @@ class PaperSearchesController < ApplicationController
       @paper_search = PaperSearch.new(params[:paper_search]).search
       @results = policy_scope(@paper_search.results || Paper.none)
       @result_count = @results.to_a.size
-      @results = @results.page(@paper_search.page).per_page(15)
+      @results = @results.page(@paper_search.page).per_page(15) unless request.format.symbol == :xls
     else
       @paper_search = PaperSearch.new
     end
@@ -12,6 +12,9 @@ class PaperSearchesController < ApplicationController
     respond_to do |format|
       format.html
       format.js
+      format.xls {
+        headers["Content-Disposition"] = "attachment; filename=\"Amaferm Research Table.xls\""
+      }
     end
   end
 end
