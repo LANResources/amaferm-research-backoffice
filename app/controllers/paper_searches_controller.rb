ee73dfaafd45a84698394bba864f3290c4d38601
Nick Reed <reednj77@gmail.com>
2018-04-06 15:03:05 -0500

    Paginate array instead of relation

--- app/controllers/paper_searches_controller.rb
+++ app/controllers/paper_searches_controller.rb
@@ -1,10 +1,12 @@
+require 'will_paginate/array'
+
 class PaperSearchesController < ApplicationController
   def search
     if params[:paper_search]
       @paper_search = PaperSearch.new(params[:paper_search]).search
       @results = policy_scope(@paper_search.results || Paper.none)
       @result_count = @results.to_a.size
-      @results = @results.page(@paper_search.page).per_page(15) unless request.format.symbol == :xls
+      @results = @results.to_a.paginate(page: @paper_search.page, per_page: 15) unless request.format.symbol == :xls
     else
       @paper_search = PaperSearch.new
     end
